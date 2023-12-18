b909e106d318af08334bfad847bee479b2a433a3
Philipp Ape <philipp.ape@outlook.de>
2023-12-18 16:27:20 +0100

    Core implementation

---
+++ BetterTasks/TaskExtensions.cs
@@ -0,0 +1,128 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BetterTasks;
+
+public static class TaskExtensions
+{
+	/// <summary>
+	/// Configures the task to continue on the captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredTaskAwaitable Captured(this Task task) =>
+		task.ConfigureAwait(ConfigureAwaitOptions.ContinueOnCapturedContext);
+	
+	/// <summary>
+	/// Configures the task to not continue on any captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredTaskAwaitable NonCaptured(this Task task) =>
+		task.ConfigureAwait(ConfigureAwaitOptions.None);
+	
+	/// <summary>
+	/// Configures the task of a specified type to continue on the captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredTaskAwaitable<T> Captured<T>(this Task<T> task) =>
+		task.ConfigureAwait(ConfigureAwaitOptions.ContinueOnCapturedContext);
+	
+	/// <summary>
+	/// Configures the task of a specified type to not continue on any captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredTaskAwaitable<T> NonCaptured<T>(this Task<T> task) =>
+		task.ConfigureAwait(ConfigureAwaitOptions.None);
+	
+	/// <summary>
+	/// Configures the value task to continue on the captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredValueTaskAwaitable Captured(this ValueTask task) =>
+		task.ConfigureAwait(true);
+	
+	/// <summary>
+	/// Configures the value task to not continue on any captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredValueTaskAwaitable NonCaptured(this ValueTask task) =>
+		task.ConfigureAwait(false);
+	
+	/// <summary>
+	/// Configures the value task of a specified type to continue on the captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredValueTaskAwaitable<T> Captured<T>(this ValueTask<T> task) =>
+		task.ConfigureAwait(true);
+	
+	/// <summary>
+	/// Configures the value task of a specified type to not continue on any captured synchronization context.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ConfiguredValueTaskAwaitable<T> NonCaptured<T>(this ValueTask<T> task) =>
+		task.ConfigureAwait(false);
+	
+	/// <summary>
+	/// Runs the task in the background and catches a specified exception type.
+	/// </summary>
+	/// <typeparam name="TException">The type of exception to catch.</typeparam>
+	/// <param name="task">The value task.</param>
+	/// <param name="onException">Action to perform when the specified exception occurs.</param>
+	/// <param name="continueOnCapturedContext">Flag to continue on the captured synchronization context.</param>
+	/// <param name="suppressThrowing">Flag to suppress throwing the caught exception.</param>
+	/// <exception cref="ArgumentNullException">Thrown when task or onException is null.</exception>
+	public static void Catch<TException>(this ValueTask task,
+		in Action<TException> onException, bool continueOnCapturedContext = false,
+		bool suppressThrowing = true) where TException : Exception
+	{
+		ArgumentNullException.ThrowIfNull(task);
+		ArgumentNullException.ThrowIfNull(onException);
+		RunBackground(task, onException, continueOnCapturedContext, suppressThrowing);
+	}
+	
+	/// <summary>
+	/// Runs the task in the background and catches a specified exception type.
+	/// </summary>
+	/// <typeparam name="TException">The type of exception to catch.</typeparam>
+	/// <param name="task">The task.</param>
+	/// <param name="onException">Action to perform when the specified exception occurs.</param>
+	/// <param name="options">Options for configuring task continuation.</param>
+	/// <param name="suppressThrowing">Flag to suppress throwing the caught exception.</param>
+	/// <exception cref="ArgumentNullException">Thrown when task or onException is null.</exception>
+	public static void Catch<TException>(this Task task,
+		in Action<TException> onException, ConfigureAwaitOptions options = ConfigureAwaitOptions.None,
+		bool suppressThrowing = true) where TException : Exception
+	{
+		ArgumentNullException.ThrowIfNull(task);
+		ArgumentNullException.ThrowIfNull(onException);
+		RunBackground(task, onException, options, suppressThrowing);
+	}
+
+	private static async void RunBackground<TException>(Task task,
+		Action<TException> onException, ConfigureAwaitOptions options,
+		bool suppressThrowing) where TException : Exception
+	{
+		try
+		{
+			await task.ConfigureAwait(options);
+		}
+		catch (TException e)
+		{
+			onException(e);
+			if (!suppressThrowing) throw;
+		}
+	}
+	
+	private static async void RunBackground<TException>(ValueTask task,
+		Action<TException> onException, bool continueOnCapturedContext,
+		bool suppressThrowing) where TException : Exception
+	{
+		try
+		{
+			await task.ConfigureAwait(continueOnCapturedContext);
+		}
+		catch (TException e)
+		{
+			onException(e);
+			if (!suppressThrowing) throw;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # BetterTasks
-The handling of tasks in C# could be better. With this package, this becomes a reality for a few aspects.
+The handling of **tasks** in C# could be **better**. With this package, this becomes a reality for a few aspects.
+
+## Overview
+This project provides extension methods for handling tasks and value tasks with different synchronization contexts and exception handling strategies.
+
+![.Net](https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white)
+
+This library only supports **.NET 8.0+**.
+
+## Usage
+
+### `Captured` and `NonCaptured` Methods
+- `Captured`: Configures a task or value task to continue on the captured synchronization context.
+- `NonCaptured`: Configures a task or value task to not continue on any captured synchronization context.
+
+### `Catch` Methods
+- `Catch<TException>(ValueTask, Action<TException>, bool, bool)`: Runs a value task in the background and catches a specified exception type, providing options to continue on the captured synchronization context and suppress throwing the caught exception.
+- `Catch<TException>(Task, Action<TException>, ConfigureAwaitOptions, bool)`: Runs a task in the background and catches a specified exception type, providing options for configuring task continuation and suppressing throwing the caught exception.
+
+## Example
+
+### Shorter variants for contextual capturing
+
+```csharp
+using BetterTasks;
+
+// Instead of:
+await DoWorkAsync().ConfigureAwait(false);
+// Or:
+await DoWorkAsync().ConfigureAwait(ConfigureAwaitOptions.None);
+// You can now write:
+await DoWorkAsync().NonCaptured();
+
+// Instead of:
+await DoWorkAsync().ConfigureAwait(true);
+// Or:
+await DoWorkAsync().ConfigureAwait(ConfigureAwaitOptions.ContinueOnCapturedContext);
+// You can now write:
+await DoWorkAsync().Captured();
+```
+
+### Managed background tasks
+
+```csharp
+using BetterTasks;
+
+DoWorkAsync().Catch<FileNotFoundException>(e =>
+{
+	Log($"Error: {e.Message}");
+});
+```
